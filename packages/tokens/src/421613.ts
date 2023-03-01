2973b872027614ebea236c2d478d6ccf6cde1358
smartdev1102 <hfish7789@gmail.com>
2023-03-01 14:54:36 +0900

    update: fix token list

--- packages/tokens/src/421613.ts
+++ packages/tokens/src/421613.ts
@@ -3,15 +3,14 @@ import { USDC_GOERLI_ARBI, USDT_GOERLI_ARBI, CAKE_GOERLI_ARBI, BUSD_GOERLI_ARBI 
 
 export const goerliArbiTestnetTokens = {
   weth: WETH9[ChainId.GOERLI_ARBI],
-  celr: new Token(
+  syrup: new Token(
     ChainId.GOERLI_ARBI,
-    '0x33d6f54e0C252623bc7541FE80860a88a1e4C40b',
+    '0x2850DfcC368ee72d7214E394F6a92BAE3a300286',
     18,
-    'Cake',
-    'PancakeSwap Token',
+    'SYRUP',
+    'SyrupBar Token',
     '',
   ),
-  leet: new Token(ChainId.GOERLI_ARBI, '0x2850DfcC368ee72d7214E394F6a92BAE3a300286', 18, 'SYRUP', 'SyrupBar Token', ''),
   usdc: USDC_GOERLI_ARBI,
   usdt: USDT_GOERLI_ARBI,
   busd: BUSD_GOERLI_ARBI,
